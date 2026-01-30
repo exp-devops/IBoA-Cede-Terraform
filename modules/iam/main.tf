eb7e3280c05667ce29fa7ffcc768af13c39c2309
bhaskar-experion <bhaskar.nandagopal@experionglobal.com>
2026-01-30 13:06:23 +0530

    changed iam for grafana

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -259,14 +259,10 @@ resource "aws_iam_role" "grafana_cloudwatch_role" {
       {
         Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::${var.grafana_account_id}:root"
+          AWS = "arn:aws:iam::${var.grafana_account_id}:role/IBoA_grafana"
         }
         Action = "sts:AssumeRole"
-        Condition = {
-          StringEquals = {
-            "sts:ExternalId" = "${data.aws_caller_identity.current.account_id}-grafana"
-          }
-        }
+        Condition = {}
       }
     ]
   })
